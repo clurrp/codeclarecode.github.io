a7b4846d35f24bb382dec07ce19ab92d24d3d316
Clare Beany <codeclarecode@gmail.com>
2024-09-23 19:06:49 -0400

    add work, skills, and about section

--- src/App.tsx
+++ src/App.tsx
@@ -3,8 +3,24 @@
  * @license Apache-2.0
  */
 
-import Header from "./components/Header";
-const App = () => { return <Header />; };
+import Header from './components/Header';
+import Hero from './components/Hero';
+import About from './components/About';
+import Skill from './components/Skill';
+import Work from './components/Work';
+
+const App = () => {
+  return (
+    <>
+      <Header />
+      <main>
+        <Hero />
+        <About />
+        <Skill />
+        <Work />
+      </main>
+    </>
+  );
+};
 
 export default App;
-
--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,18 +1,36 @@
+// commponents
+
+import Navbar from './Navbar';
+import { useState } from 'react';
+
 const Header = () => {
+  const [navOpen, setNavOpen] = useState(false);  
   return (
     <header className="fixed top-0 left-0 w-full h-20 flex items-center z-40 bg-gradient-to-b from-zinc-900 to-zinc-900/0">
-      <div className="max-w-screen-2 w-full mx-auto px-4">
+      <div className="max-w-screen-2xl w-full mx-auto px-4 flex justify-between items-center md:px-6 md:grid md:grid-cols-[1fr,3fr,1fr]">
         <h1>
+          {/*  Replace below with logo */}
           <a href="/" className="logo">
-            codewithclare <img src="/images/cat.svg" width={40} height={40} alt="Clare Beany" />
+            codewithclare{' '}
+            <img
+              src="/images/cat.svg"
+              width={40}
+              height={40}
+              alt="Clare Beany"
+            />
           </a>
         </h1>
 
-        <div className="">
-          <button className="" onClick={() => {}}>
-            <span className="material-symbols-rounded">Menu</span>
+        <div className="relative md:justify-self-center">
+          <button className="menu-btn md:hidden" onClick={() => setNavOpen((prev) => !prev)}>
+            <span className="material-symbols-rounded">{ navOpen ? 'close' : 'menu'}</span>
           </button>
-          </div>
+          <Navbar navOpen={navOpen}/>
+        </div>
+
+        <a href="#contact" className="btn btn-secondary max-md:hidden md:justify-self-end">
+          Contact Menu
+        </a>
       </div>
     </header>
   );
